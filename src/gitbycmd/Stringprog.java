38a90c39ebbd7cb5bf69fbe3da28947e79b793f4
p80133175 <38097718+p80133175@users.noreply.github.com>
2018-10-03 13:47:39 -0700

    Update Stringprog.java

--- src/gitbycmd/Stringprog.java
+++ src/gitbycmd/Stringprog.java
@@ -4,7 +4,7 @@ public class Stringprog {
 
 	public static void main(String[] args) {
 		System.out.println("my string prog");
-		System.out.println("updated in git");
+		System.out.println("2nd update");
 
 	}
 
